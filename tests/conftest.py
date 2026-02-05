7e08ee4e064fcf269857838cd9415f8398d669af
Caue Casonato <caueroriz@gmail.com>
2026-02-05 11:09:27 -0800

    FIX: Correct test workflow

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,13 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope="function")
 def driver():
     options = Options()
-    options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
+
+    if os.getenv("HEADLESS") == "true":
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
     
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
