5db55997c8d694aa34e247ea7e012aeec14277e3
Caue Casonato <caueroriz@gmail.com>
2026-02-05 11:14:58 -0800

    Fix ModuleNotFoundError by adding 'os' to imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import os
 
 @pytest.fixture(scope="function")
 def driver():
